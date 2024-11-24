79f34b930de1ea2f52f90241c939921ec974f87e
STEN <sten.li@foxmail.com>
2024-11-24 09:13:57 +0800

    update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,6 +30,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-cycle': 'off',
     'import/no-named-as-default-member': 'off',
     'import/no-named-as-default': 'off',
     'import/prefer-default-export': 'off',
