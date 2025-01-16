aac0ff710029ab39fcda171f2b46f92804375f97
STEN <sten.li@foxmail.com>
2025-01-16 16:51:42 +0800

    Update useEventBus.js

--- src/composables/useEventBus.js
+++ src/composables/useEventBus.js
@@ -21,67 +21,6 @@ export function useEventBus(handlers, loadDataStore) {
   onMounted(async () => {
     if (handlers) {
       window.$bus = bus;
-      if (window.parent === window) {
-        // window['__ports__'] = ports;
-        window.addEventListener('message', (e) => {
-          if (e.source === window && e.data?.type === 'response-message-port') {
-            const { peer1, peer2 } = e.data.payload;
-            if (peerId === peer1) {
-              ports[peer2] = e.ports[0];
-              const port = ports[peer2];
-              port.onclose = () => {
-                delete ports[peer2];
-              };
-              port.onmessage = (event) => {
-                const { type, payload } = event.data;
-                switch (type) {
-                  // ...
-                  default:
-                    console.log(payload);
-                    break;
-                }
-              };
-            }
-            if (peerId === peer2) {
-              ports[peer1] = e.ports[0];
-              const port = ports[peer1];
-              port.onclose = () => {
-                delete ports[peer1];
-              };
-              port.onmessage = (event) => {
-                const { type, payload } = event.data;
-                switch (type) {
-                  case 'load': {
-                    window.$bus.emitter.emit(type, payload);
-                    break;
-                  }
-                  case 'unload': {
-                    window.$bus.emitter.emit(type);
-                    break;
-                  }
-                  case 'unselect': {
-                    window.$bus.emitter.emit(type);
-                    break;
-                  }
-                  // ...
-                  default:
-                    break;
-                }
-              };
-            }
-          }
-        })
-        while (!window.$electron) {
-          // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
-          await new Promise(r => setTimeout(r, 1000));
-        }
-        if (window.$electron && projectId) {
-          window.$electron.requestMessagePort({
-            peer1: peerId.replace('volview-', 'tab-project-'),
-            peer2: peerId,
-          });
-        }
-      }
     } else {
       return;
     }
@@ -147,6 +86,65 @@ export function useEventBus(handlers, loadDataStore) {
       // window.addEventListener('message', e => // ...
       window.parent.postMessage('volview:LOAD', '*');
     } else {
+      // window['__ports__'] = ports;
+      window.addEventListener('message', (e) => {
+        if (e.source === window && e.data?.type === 'response-message-port') {
+          const { peer1, peer2 } = e.data.payload;
+          if (peerId === peer1) {
+            ports[peer2] = e.ports[0];
+            const port = ports[peer2];
+            port.onclose = () => {
+              delete ports[peer2];
+            };
+            port.onmessage = (event) => {
+              const { type, payload } = event.data;
+              switch (type) {
+                // ...
+                default:
+                  console.log(payload);
+                  break;
+              }
+            };
+          }
+          if (peerId === peer2) {
+            ports[peer1] = e.ports[0];
+            const port = ports[peer1];
+            port.onclose = () => {
+              delete ports[peer1];
+            };
+            port.onmessage = (event) => {
+              const { type, payload } = event.data;
+              switch (type) {
+                case 'load': {
+                  window.$bus.emitter.emit(type, payload);
+                  break;
+                }
+                case 'unload': {
+                  window.$bus.emitter.emit(type);
+                  break;
+                }
+                case 'unselect': {
+                  window.$bus.emitter.emit(type);
+                  break;
+                }
+                // ...
+                default:
+                  break;
+              }
+            };
+          }
+        }
+      })
+      while (!window.$electron) {
+        // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
+        await new Promise(r => setTimeout(r, 1000));
+      }
+      if (window.$electron && projectId) {
+        window.$electron.requestMessagePort({
+          peer1: peerId.replace('volview-', 'tab-project-'),
+          peer2: peerId,
+        });
+      }
       console.log('[volview]', 'mounted!');
     }
   });
