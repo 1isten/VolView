ce0e4eab7d2cf6b5c9de7bb70ca4a6f035696488
STEN <sten.li@foxmail.com>
2025-03-11 01:44:57 +0800

    resolve conflicts

--- src/components/App.vue
+++ src/components/App.vue
@@ -132,14 +132,18 @@ export default defineComponent({
     const { currentImageMetadata, isImageLoading } = useCurrentImage();
     const defaultImageMetadataName = defaultImageMetadata().name;
     watch(currentImageMetadata, (newMetadata) => {
+      let prefix = '';
       if (
         newMetadata?.name &&
         // wait until we get a real name, but if we never do, show default name
         (newMetadata.name !== defaultImageMetadataName || !isImageLoading)
       ) {
-        // document.title = `${newMetadata.name} - VolView`;
+        prefix = `${newMetadata.name} -`;
       }
+      document.title = `${prefix}VolView`;
     });
+
+    // ---
 
     // --- event handling --- //
     /*
