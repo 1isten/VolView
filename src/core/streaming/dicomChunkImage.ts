1isten/VolView

commit eeba2a46816faaaae0c7ce5d8028a1d9e3fdc9e1

fix cases when chunk dataBlob is not loaded

src/core/streaming/dicomChunkImage.ts
@@ -303,18 +303,14 @@ export default class DicomChunkImage
         const seriesFiles: File[] = []
         let seriesFilesCompleted = true;
         this.chunks.forEach(async (chunk, chunkIndex) => {
+          if (chunk.state === ChunkState.MetaOnly || !chunk.dataBlob) {
+            await chunk.loadData();
+          }
           if (chunk.metaBlob) {
-            const file = chunk.metaBlob as File;
+            const file: File = chunk.metaBlob instanceof File ? chunk.metaBlob : new File([chunk.metaBlob], `file-${chunkIndex}.dcm`, { type: chunk.metaBlob.type } );
             seriesFiles[chunkIndex] = file;
             return;
           }
-          /*
-          if (chunk.dataLoader?.fetcher?.chunks) {
-            const file = new File(chunk.dataLoader.fetcher.chunks, `file-${chunkIndex}.dcm`, { type: chunk.dataLoader.fetcher.contentType } );
-            seriesFiles.push(file);
-            return;
-          }
-          */
           seriesFilesCompleted = false;
         });
         if (seriesFiles.length > 0 && seriesFilesCompleted) {

commit e21a5690b618abc2eb697aad783b18e7e85a996b

fix bug caused by eeba2a46

src/core/streaming/dicomChunkImage.ts
@@ -303,7 +303,7 @@ export default class DicomChunkImage
         const seriesFiles: File[] = []
         let seriesFilesCompleted = true;
         this.chunks.forEach(async (chunk, chunkIndex) => {
-          if (chunk.state === ChunkState.MetaOnly || !chunk.dataBlob) {
+          if (chunk.state === ChunkState.MetaOnly && !chunk.dataBlob) {
             await chunk.loadData();
           }
           if (chunk.metaBlob) {